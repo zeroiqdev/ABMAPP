9706f00298f4656d76b317a58e30750afb5a5fd0
zeroiqdev <allenbloo2@gmail.com>
2026-01-10 06:18:45 +0100

    env keys updated on expo

--- app.config.js
+++ app.config.js
@@ -38,7 +38,7 @@ export default {
   expo: {
     name: "ABM Workshop & Marketplace",
     slug: "abmapp",
-    version: "1.0.4",
+    version: "1.0.5",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
@@ -67,7 +67,7 @@ export default {
         backgroundColor: "#ffffff"
       },
       package: "com.abmtek.abmapp",
-      versionCode: 5,
+      versionCode: 6,
       googleServicesFile: './google-services.json',
       config: {
         googleMaps: {
