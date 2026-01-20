2ec3d2f4f102b248b7ba287a6fb8b9e4a6f32789
zeroiqdev <allenbloo2@gmail.com>
2026-01-20 14:28:21 +0100

    version bump

--- app.config.js
+++ app.config.js
@@ -38,7 +38,7 @@ export default {
   expo: {
     name: "ABM Workshop & Marketplace",
     slug: "abmapp",
-    version: "1.0.8",
+    version: "1.0.9",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "automatic",
@@ -57,7 +57,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.abmtek.abmapp",
-      buildNumber: "9",
+      buildNumber: "10",
       backgroundColor: "#000000",
       googleServicesFile: './GoogleService-Info.plist',
       config: {
@@ -71,7 +71,7 @@ export default {
         backgroundColor: "#000000"
       },
       package: "com.abmtek.abmapp",
-      versionCode: 9,
+      versionCode: 10,
       googleServicesFile: './google-services.json',
       config: {
         googleMaps: {
