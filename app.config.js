6fe7a1d60e70d426ef5636f99b68c82ca3703413
zeroiqdev <allenbloo2@gmail.com>
2026-04-01 07:34:31 +0100

    build: synchronize version to 1.0.24 (build 25) to resolve Play Store rollout error

--- app.config.js
+++ app.config.js
@@ -38,7 +38,7 @@ export default {
   expo: {
     name: "ABM Workshop & Marketplace",
     slug: "abmapp",
-    version: "1.0.23",
+    version: "1.0.24",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "automatic",
@@ -54,7 +54,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.abmtek.abmapp",
-      buildNumber: "24",
+      buildNumber: "25",
       backgroundColor: "#000000",
       usesAppleSignIn: true,
       googleServicesFile: './GoogleService-Info.plist',
@@ -69,7 +69,7 @@ export default {
         backgroundColor: "#000000"
       },
       package: "com.abmtek.abmapp",
-      versionCode: 24,
+      versionCode: 25,
       googleServicesFile: './google-services.json',
       config: {
         googleMaps: {
